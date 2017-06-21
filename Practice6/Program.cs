d06513ec10b364bcc6eb36dcb8c27290fc107114
IskusnikXD <IskusnikXD@ISKUSNIK>
2017-06-21 17:23:05 +0500

    1. Спросить, подпоследовательность искать на первых N элементах, или на всей возможной последовательности.

--- Practice6/Program.cs
+++ Practice6/Program.cs
@@ -8,8 +8,31 @@ namespace Practice6
 {
     class Program
     {
+        /// <summary>
+        /// 14.	
+        /// Ввести а1, а2, а3, N. 
+        /// Построить последовательность чисел а[к] = ( а[к–1] + а[к-2] ) / 2 –  а[к–3]. 
+        /// Построить N элементов последовательности. 
+        /// Определить длину максимальной возрастающей подпоследовательности. 
+        /// Напечатать последовательность, длину и последний элемент максимальной возрастающей подпоследовательности. 
+        /// </summary>
+        /// 
         static void Main(string[] args)
         {
+            int a1, a2, a3, N;
+            Console.WriteLine("а[к] = (а[к–1] + а[к-2]) / 2 – а[к–3]");
+
+            Console.WriteLine("a1 =");
+            a1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("a2 =");
+            a2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("a3 =");
+            a3 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("N =");
+            N = int.Parse(Console.ReadLine());
         }
     }
 }
